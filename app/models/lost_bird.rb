7d2387b06edc9f7fa89bd7f4e808cebfb41eed33
itakurara <kaiamira26@gmail.com>
2019-06-19 12:13:38 +0900

    elasticsearchのマッピングを設定

--- app/models/lost_bird.rb
+++ app/models/lost_bird.rb
@@ -1,15 +1,17 @@
 class LostBird < ApplicationRecord
   include Elasticsearch::Model
 
-  # TODO:
-  # mapping dynamic: 'strict' do
-  #   indexes :description, analyzer: 'kuromoji', type: 'text'
-  #   indexes :kind, analyzer: 'kuromoji', type: 'text'
-  #   indexes :lost_at, type: 'date'
-  #   indexes :found_at, type: 'date'
-  #   indexes :status, type: 'keyword'
-  #   indexes :place, type: 'text'
-  # end
+  mapping dynamic: 'strict' do
+    indexes :description, analyzer: 'kuromoji', type: 'text'
+    indexes :kind, analyzer: 'kuromoji', type: 'text'
+    indexes :lost_at, type: 'date'
+    indexes :found_at, type: 'date'
+    indexes :ring_number, analyzer: 'kuromoji', type: 'text'
+    indexes :name, analyzer: 'kuromoji', type: 'text'
+    indexes :address, analyzer: 'kuromoji', type: 'text'
+    indexes :status, type: 'keyword'
+    indexes :resolved, type: 'boolean'
+  end
 
   belongs_to :bird
   enum status: [:lost, :kept]
@@ -24,8 +26,17 @@ class LostBird < ApplicationRecord
     end
   end
 
-  # TODO:
-  # def as_indexed_json(options={})
-  #   self.as_json(only: 'description')
-  # end
+  def as_indexed_json(options={})
+    {
+      description: description,
+      kind: bird.ja_name,
+      lost_at: lost_at,
+      found_at: found_at,
+      ring_number: ring_number,
+      name: name,
+      address: address,
+      status: status,
+      resolved: resolved
+    }
+  end
 end
