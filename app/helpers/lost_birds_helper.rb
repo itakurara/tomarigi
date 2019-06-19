d01d46696b7b72954d72f041d342aa0faa1f56a3
itakurara <kaiamira26@gmail.com>
2019-06-19 10:14:33 +0900

    viewに書いてたロジックをhelperに移動

---
+++ app/helpers/lost_birds_helper.rb
@@ -0,0 +1,9 @@
+module LostBirdsHelper
+  def options_for_status
+    LostBird.statuses.map { |k, v| [LostBird.human_attribute_name("status.#{k}"), v] }
+  end
+
+  def options_for_birds
+    Bird.all.map { |b| [b.ja_name, b.id] }
+  end
+end
