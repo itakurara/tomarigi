ad2f0393e5b17cf18f4e14df544895a56d89c55d
itakurara <kaiamira26@gmail.com>
2019-06-25 20:48:12 +0900

    herokuにBONSAI導入

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,2 @@
-config = {
-  host: 'http://localhost:9200'
-}
-
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
+url = ENV['BONSAI_URL'] || 'http://localhost:9200'
+Elasticsearch::Model.client = Elasticsearch::Client.new(url: url)
