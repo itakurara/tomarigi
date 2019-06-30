c5f852647eb9cf71a70afc165b034a81d7d30130
itakurara <kaiamira26@gmail.com>
2019-06-30 18:02:24 +0900

    コメントを削除できるようにした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = Comment.find(params.permit(:id)[:id])
+    comment.destroy!
+
+    redirect_to lost_bird_path(comment.lost_bird_id)
+  end
+
   private
 
   def comment_params
