10cab8da8891725ee75190f791ef6f21e0c78fa9
itakurara <kaiamira26@gmail.com>
2019-07-07 21:42:08 +0900

    SendGridの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'tomarigi.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'tomarigi.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
